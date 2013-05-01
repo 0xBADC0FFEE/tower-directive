673ce9217595d0d76d4beba261de77809525c7d4
Binh Tran <btbinhtran@gmail.com>
2013-05-01 16:35:39 -0700

    Added tests and testem

--- test/index.js
+++ test/index.js
@@ -1,6 +1,39 @@
-var binding = require('..')
+var binding = 'undefined' == typeof window ? require('..') : require('tower-data-binding')
   , assert = require('assert');
 
 describe("data-binding", function(){
+  var bindObj = {};
 
+  beforeEach(function() {
+    binding(bindObj);
+  });
+
+  describe("changed", function() {
+    it('should emit `changed` event', function(done) {
+      bindObj.on('changed', function(key) {
+        assert('name' === key);
+        done();
+      });
+
+      bindObj.changed(['name']);
+    });
+
+    it('should emit `(key) changed` event', function(done) {
+      bindObj.on('name changed', function(key) {
+        done();
+      });
+
+      bindObj.changed(['name']);
+    });
+  });
+
+  describe('propagateBindings', function() {
+    it('should emit `propagating bindings` event', function(done) {
+      bindObj.on('propagating bindings', function() {
+        done();
+      });
+
+      bindObj.propagateBindings();
+    });
+  });
 });
